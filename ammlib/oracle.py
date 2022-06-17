cb9ef9288b3f88e2a95ef4029b18460303ba2e23
David Bouba <d.m.bouba@gmail.com>
2022-06-17 18:19:07 +0200

    feat(oracle): added sparse oracle + historical prices getter

--- ammlib/oracle.py
+++ ammlib/oracle.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class BaseOracle:
 
     def __init__(self, ):
@@ -19,6 +20,17 @@ class BaseOracle:
 
     def get(self, current_time):
         return np.nan
+
+    def get_last_timestamped_prices(self, lookback_calls, route, lookback_step=1):
+        if not self.length:
+            return []
+        return [
+            {
+                "price": self.prices[-i * lookback_step - 1] if route == "01" else 1 / self.prices[-i * lookback_step - 1],
+                "ts": self.times[-i * lookback_step - 1]
+            }
+            for i in range(min(int((self.length - 1) / lookback_step), lookback_calls))
+        ]
 
 
 class PerfectOracle(BaseOracle):
@@ -41,4 +53,18 @@ class LaggedOracle(BaseOracle):
         while (self.times[self.length-1-t] > current_time - self.lag) and (t < self.length-1):
             t += 1
         return self.prices[self.length-1-t]
+
+    def get_last_timestamped_prices(self, lookback_calls, route, lookback_step=1):
+        return []
+
+
+class SparseOracle(PerfectOracle):
+
+    def __init__(self, min_period):
+        super().__init__()
+        self.min_period = min_period
+
+    def update(self, time, price):
+        if (not self.times) or (time >= self.times[-1] + self.min_period):
+            super().update(time, price)
 
