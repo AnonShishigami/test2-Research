46957db6168e3c4aab1cb961eac67ad8e04221c4
David Bouba <d.m.bouba@gmail.com>
2022-06-17 18:20:20 +0200

    feat(lp): added swaap lp impl

--- ammlib/liquidityprovider.py
+++ ammlib/liquidityprovider.py
@@ -1,6 +1,6 @@
 import numpy as np
 from .logistictools import LogisticTools
-import matplotlib.pyplot as plt
+
 
 class BaseLiquidityProvider:
 
@@ -20,7 +20,6 @@ class BaseLiquidityProvider:
         self.last_cashed_01 = None
         self.last_cashed_10 = None
 
-
     def reset(self):
 
         self.inventories = self.initial_inventories.copy()
@@ -34,7 +33,6 @@ class BaseLiquidityProvider:
         return np.inf, 0.
 
     def proposed_swap_prices_01(self, time, nb_coins_1):
-
         self.last_requested_nb_coins_1 = nb_coins_1
         swap_price_01 = self.oracle.get(time)
         answer, cashed = self.pricing_function_01(nb_coins_1, swap_price_01)
@@ -44,7 +42,6 @@ class BaseLiquidityProvider:
         return answer
 
     def proposed_swap_prices_10(self, time, nb_coins_0):
-
         self.last_requested_nb_coins_0 = nb_coins_0
         swap_price_10 = 1. / self.oracle.get(time)
         answer, cashed = self.pricing_function_10(nb_coins_0, swap_price_10)
@@ -100,13 +97,21 @@ class LiquidityProviderAMMSqrt(BaseLiquidityProvider):
         if self.inventories[1] <= nb_coins_1:
             return np.inf, 0.
         else:
-            return self.inventories[0] / (self.inventories[1] - nb_coins_1) / (1. - self.delta), 0.
+            return self.get_cpmm_price(
+                self.inventories[0], 0.5, self.inventories[1], 0.5, nb_coins_1, self.delta
+            ), 0.
 
     def pricing_function_10(self, nb_coins_0, swap_price_10):
         if self.inventories[0] <= nb_coins_0:
             return np.inf, 0.
         else:
-            return self.inventories[1] / (self.inventories[0] - nb_coins_0) / (1. - self.delta), 0.
+            return self.get_cpmm_price(
+                self.inventories[1], 0.5, self.inventories[0], 0.5, nb_coins_0, self.delta
+            ), 0.
+
+    @staticmethod
+    def get_cpmm_price(r_in, w_in, r_out, w_out, amount_out, delta):
+        return (r_in * ((r_out / (r_out - amount_out)) ** (w_out / w_in) - 1)) / (amount_out * (1. - delta))
 
 
 class LiquidityProviderBestClosedForm(BaseLiquidityProvider):
@@ -134,7 +139,7 @@ class LiquidityProviderBestClosedForm(BaseLiquidityProvider):
         else:
             hodl_spread = (self.inventories[1] - self.initial_inventories[1]) * swap_price_01
             size = nb_coins_1 * swap_price_01
-            z_index =  np.argmin(np.abs(self.market.sizes-size))
+            z_index = np.argmin(np.abs(self.market.sizes-size))
             p = -2. * self.a * hodl_spread - self.b + size * self.a
             delta = self.lts_01[z_index].delta(p)
             return swap_price_01 * (1. + delta), delta * nb_coins_1 * swap_price_01
@@ -152,3 +157,171 @@ class LiquidityProviderBestClosedForm(BaseLiquidityProvider):
             delta = self.lts_10[z_index].delta(p)
 
         return swap_price_10 * (1. + delta), delta * nb_coins_0
+
+
+class LiquidityProviderSwaapV1(LiquidityProviderAMMSqrt):
+
+    def __init__(
+        self, name, initial_inventories, initial_cash, market, oracle, delta,
+        z, horizon, lookback_calls, lookback_step,
+    ):
+        super().__init__(name, initial_inventories, initial_cash, market, oracle, delta)
+        self.delta = delta
+        self.initial_price = None
+        self.initial_weights = {
+            0: 0.5,
+            1: 0.5
+        }
+        self.lookback_calls = lookback_calls
+        self.lookback_step = lookback_step
+        self.horizon = horizon
+        self.z = z
+
+    def set_init_price(self, time):
+        self.initial_price = {
+            1: self.oracle.get(time)
+        }
+        self.initial_price[0] = 1 / self.initial_price[1]
+
+    def proposed_swap_prices_01(self, time, nb_coins_1):
+        if self.initial_price is None:
+            self.set_init_price(time)
+        return super().proposed_swap_prices_01(time, nb_coins_1)
+
+    def proposed_swap_prices_10(self, time, nb_coins_0):
+        if self.initial_price is None:
+            self.set_init_price(time)
+        return super().proposed_swap_prices_10(time, nb_coins_0)
+
+    def pricing_function_01(self, nb_coins_1, swap_price_01):
+        return self.pricing_function_wrapper(
+            nb_coins_1,
+            swap_price_01,
+            1
+        )
+
+    def pricing_function_10(self, nb_coins_0, swap_price_10):
+        return self.pricing_function_wrapper(
+            nb_coins_0,
+            swap_price_10,
+            0
+        )
+
+    def pricing_function_wrapper(self, amount_out, price, index_out):
+        index_in = 0 if index_out == 1 else 1
+        dynamic_weights = self.get_dynamic_weights(price, index_out)
+        price = self.pricing_function(
+            self.inventories[index_in],
+            index_in,
+            self.inventories[index_out],
+            index_out,
+            dynamic_weights,
+            amount_out,
+            price,
+            self.delta
+        )
+        return price, 0.
+
+    def pricing_function(self, r_in, index_in, r_out, index_out, dynamic_weights, amount_out, price_out, delta):
+        if r_out <= amount_out:
+            return np.inf, 0.
+        else:
+            # weights dynamically adjusted according to assets's relative performance
+
+            # defines abundance / shortage boundary
+            r_out_at_oracle_price = self.get_in_reserve_at_price(
+                r_out, dynamic_weights[index_out],
+                r_in, dynamic_weights[index_in],
+                1 / price_out
+            )
+            if r_out - amount_out > r_out_at_oracle_price:
+                # abundance phase only
+                return self.get_cpmm_price(
+                    r_in, dynamic_weights[index_in],
+                    r_out, dynamic_weights[index_out],
+                    amount_out,
+                    delta
+                )
+
+            # will experience shortage phase
+            spread_factor = self.get_spread_factor(route=f"{index_in}{index_out}")
+
+            if r_out > r_out_at_oracle_price:
+                # abundance before shortage phase
+                a_amount = r_out - r_out_at_oracle_price
+                a_price = self.get_cpmm_price(
+                    r_in, dynamic_weights[index_in],
+                    r_out, dynamic_weights[index_out],
+                    a_amount,
+                    delta
+                )
+                # shortage phase
+                dynamic_weights = self.apply_spread(dynamic_weights, index_out, spread_factor)
+                s_amount = amount_out - a_amount
+                s_price = self.get_cpmm_price(
+                    r_in + a_price * a_amount, dynamic_weights[index_in],
+                    r_out - a_amount, dynamic_weights[index_out],
+                    s_amount,
+                    delta
+                )
+                return (a_price * a_amount + s_price * s_amount) / (a_amount + s_amount)
+
+            # only shortage
+            dynamic_weights = self.apply_spread(dynamic_weights, index_out, spread_factor)
+            return self.get_cpmm_price(
+                r_in, dynamic_weights[index_in],
+                r_out, dynamic_weights[index_out],
+                amount_out,
+                delta
+            )
+
+    def get_dynamic_weights(self, running_price, index):
+        weights = dict((k, self.initial_weights[k]) for k in self.initial_weights)
+        weights[index] *= running_price / self.initial_price[index]
+        return self.normalize_weights(weights)
+
+    def apply_spread(self, weights, index_out, spread_factor):
+        _weights = dict((k, weights[k]) for k in weights)
+        _weights [index_out] *= spread_factor
+        return self.normalize_weights(_weights)
+
+    @staticmethod
+    def normalize_weights(weights):
+        w = np.sum(list(weights.values()))
+        return dict((k, weights[k] / w) for k in weights)
+
+    # GBM spread computation
+    def get_spread_factor(self, route):
+
+        # gets historical prices
+        seq = self.oracle.get_last_timestamped_prices(
+            lookback_calls=self.lookback_calls,
+            route=route,
+            lookback_step=self.lookback_step
+        )
+        if len(seq) < 2:
+            return 1
+        n = len(seq)
+        log_diff = np.log(seq[0]["price"] / seq[-1]["price"])
+        ts_diff = seq[0]["ts"] - seq[-1]["ts"]
+
+        # computes GBM MLE
+        mean = log_diff / ts_diff
+        variance = (
+            np.sum(
+                [
+                    np.log(seq[i]["price"] / seq[i + 1]["price"]) ** 2 / (seq[i]["ts"] - seq[i + 1]["ts"])
+                    for i in range(0, n - 1)
+                ]
+            )
+            - log_diff ** 2 / ts_diff
+        ) / n
+
+        # computes GBM quantile
+        spread_factor = np.exp(mean * self.horizon + self.z * np.sqrt(2 * variance * self.horizon))
+        return max(1, spread_factor)
+
+    @staticmethod
+    def get_in_reserve_at_price(r_in, w_in, r_out, w_out, price):
+        r_in_eq = ((price * w_in / w_out * r_out) ** w_out) * r_in ** w_in
+        return r_in_eq
